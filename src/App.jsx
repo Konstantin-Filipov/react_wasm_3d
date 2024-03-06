0c21b42d9128e757f1a55ec7d4eab2afa60e9a3a
Konstantin-Filipov <kosio1234545@gmail.com>
2024-03-06 22:36:20 +0100

    remove css

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,84 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import SceneInit from './SceneInit';
+import init, { run_bevy_app } from '../wasm_3d/pkg/wasm_3d';
 //import Menu from './components/Menu'
 //import './App.css'
 
-function App(){
-    //bufferGeometry states
-    const [bufferGeometry, setBufferGeometry] = useState(null);
-
-    //loading the object state 
-    const [isLoading, setIsLoading] = useState(true);
+function App() {
+    const [model, setModel] = useState(null)
+    const [jsLoadTime, setJsLoadTime] = useState(null)
+    const [wasmLoadTime, setWasmLoadTime] = useState(null)
 
     useEffect(() => {
-        load3d()
-    }, [])
+        init().then(() => {
+            console.log('Wasm module initialized');
+        });
+    }, []);
+
 
     const onJsBtnCLick = () => {
-        if (bufferGeometry) {
-            const startTime = performance.now();
-            
-            //call calculate center JSfunction
-            calculateGeometricCenter(bufferGeometry);
-            
-            const endTime = performance.now();
-            console.log(`Loading time: ${endTime - startTime}`)
-        } else {
-            console.error('Buffer geometry not loaded yet.');
-        }
-    }
-
-    const onWasmBtnCLick = () => {
         const startTime = performance.now();
-        // Call WASM function here
+        load3d();        
         const endTime = performance.now();
-        console.log("WASM load performance: ", endTime - startTime)
+        setJsLoadTime(endTime-startTime)
     }
+
+    const onWasmBtnCLick = async () => {
+        const startTime = performance.now();
+        try {
+            run_bevy_app();
+        } catch (error) {
+            
+        }
+        const endTime = performance.now();
+        setWasmLoadTime(endTime-startTime)
+    }
+
     
 
-    function calculateGeometricCenter(bufferGeometry) {
-        if (!bufferGeometry.isBufferGeometry) {
-            console.error('Geometry is not BufferGeometry.');
-            return new THREE.Vector3();
-        }
-    
-        const positionAttribute = bufferGeometry.getAttribute('position');
-        const vertexCount = positionAttribute.count;
-        let centroid = new THREE.Vector3(0, 0, 0);
-    
-        for (let i = 0; i < vertexCount; i++) {
-            centroid.x += positionAttribute.getX(i);
-            centroid.y += positionAttribute.getY(i);
-            centroid.z += positionAttribute.getZ(i);
-        }
-    
-        centroid.divideScalar(vertexCount);
-    
-        console.log('Geometric Center:', centroid);
-        return centroid;
-    }
-    
 
     const load3d = () => {
         const test = new SceneInit('myThreeJsCanvas');
         test.initialize();
         test.animate();
-    
+
+        let loadedModel;
         const glftLoader = new GLTFLoader();
         glftLoader.load('/src/assets/truck.glb', (gltfScene) => {
-            const loadedModel = gltfScene.scene;
-            loadedModel.name = "object" // set object name
-
+            loadedModel = gltfScene;
             // Calculate bounding box
-            const boundingBox = new THREE.Box3().setFromObject(loadedModel);
+            const boundingBox = new THREE.Box3().setFromObject(gltfScene.scene);
             const size = new THREE.Vector3();
             boundingBox.getSize(size);
-            
+
+            setModel(gltfScene)
 
             // Calculate scale factor based on the maximum dimension of the bounding box
             const scaleFactor = 10 / Math.max(size.x, size.y, size.z);
 
             // Apply scale to the model
-            loadedModel.scale.set(scaleFactor, scaleFactor, scaleFactor);
+            gltfScene.scene.scale.set(scaleFactor, scaleFactor, scaleFactor);
 
-    
-            loadedModel.rotation.y = Math.PI / 8;
-            //gltfScene.scene.position.y = 0;
-            //gltfScene.scene.scale.set(8, 8, 8)
-            
-            // Traverse the loaded model
-            loadedModel.traverse((child) => {
-                // Check if the child is a mesh and has geometry
-                if (child.isMesh && child.geometry) {
-                    
-                    //print information about the geometry
-                    console.log("Geometry name:", child.name);
-                    console.log("Geometry:", child.geometry);
-                    
-                    //set states
-                    setBufferGeometry(child.geometry);
-                    setIsLoading(false);
-                }
-            });
-            test.scene.add(loadedModel);
+
+            gltfScene.scene.rotation.y = Math.PI / 8;
+
+            test.scene.add(gltfScene.scene);
         });
     }
-    
+
     return (
-        <div>
-            <button onClick={onJsBtnCLick} disabled={isLoading}>Process with JS</button>
-            <button>Process with Wasm</button>
-            <canvas id="myThreeJsCanvas"/>
+        <div className='App'>
+            <div>
+            <button onClick={onJsBtnCLick}>Process with JS</button>
+            <button onClick={onWasmBtnCLick}>Process with Wasm</button>
+            <h3>JS load time: {jsLoadTime}</h3>
+            <h3>Wasm load time: {wasmLoadTime}</h3>
+            </div>
+
+            <canvas id="myThreeJsCanvas" />
         </div>
     );
 }
